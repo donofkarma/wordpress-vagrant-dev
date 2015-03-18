9e12f22951df5e62256410fd824636bc8ca6a84c
Jasal Vadgama <jasal@blacklabelcreative.com>
2015-03-18 17:23:18 +0000

    reordering dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -157,9 +157,9 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-sass');
 
     // // Default task(s)
     grunt.registerTask('test', ['jshint']);
