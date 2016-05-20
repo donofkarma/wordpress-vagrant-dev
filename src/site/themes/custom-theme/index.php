7041c9e9b9b61b220e9e20ce6d76215245cba788
Jasal Vadgama <jasal@blacklabelcreative.com>
2016-05-20 10:51:21 +0100

    adding single template. adding comments layout.

--- src/site/themes/custom-theme/index.php
+++ src/site/themes/custom-theme/index.php
@@ -18,12 +18,55 @@ if ( !class_exists( 'Timber' ) ) {
     return;
 }
 
-$context = Timber::get_context();
-$context['posts'] = Timber::get_posts();
+
 $templates = array( 'index.twig' );
-// if ( is_home() ) {
-//     array_unshift( $templates, 'home.twig' );
-// }
+$context = Timber::get_context();
+
+if ( is_singular() ) {
+    $context['post'] = new TimberPost();
+} else {
+    $context['posts'] = Timber::get_posts();
+}
+
+if ( is_home() || is_front_page() ) {
+    array_unshift( $templates, 'home.twig' );
+} else if ( is_single() ) {
+    array_unshift( $templates, 'single-' . $post->post_name . '.twig', 'single.twig' );
+} else if ( is_page() ) {
+    array_unshift( $templates, 'page-' . $post->post_name . '.twig', 'page.twig' );
+} else if ( is_archive() ) {
+    // archives
+
+    // add the default archive template
+    array_unshift( $templates, 'archive.twig' );
+
+    if ( is_day() ) {
+        $context['title'] = 'Archive: ' . get_the_date( 'D M Y' );
+    } else if ( is_month() ) {
+        $context['title'] = 'Archive: ' . get_the_date( 'M Y' );
+    } else if ( is_year() ) {
+        $context['title'] = 'Archive: ' . get_the_date( 'Y' );
+    } else if ( is_category() ) {
+        array_unshift( $templates, 'category-' . get_query_var( 'cat' ) . '.twig', 'category.twig' );
+
+        $data['archive_title'] = get_cat_name( get_query_var('cat') );
+    } else if ( is_tag() ) {
+        array_unshift( $templates, 'tag.twig' );
+
+        $data['archive_title'] = get_term_name( get_query_var('tag_id') );
+    } else if ( is_author() ) {
+        array_unshift( $templates, 'author.twig' );
+
+        $data['archive_title'] = 'Author Archives: ' . get_the_author();
+    } else if ( is_post_type_archive() ) {
+        array_unshift( $templates, 'archive-' . get_post_type() . '.twig' );
+
+        $context['title'] = post_type_archive_title( '', false );
+    }
+}
+
+// var_dump( $templates );
+
 Timber::render( $templates, $context );
 
 ?>
